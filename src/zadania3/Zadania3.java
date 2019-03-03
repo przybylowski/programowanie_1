e1d41212020764fb5e0ddeedaa9ad53ed8e554f5
Gilbert Przybylowski <gilbert.przybylowski@gmail.com>
2019-03-03 16:01:04 +0100

    sprawdzenie palindromu v1

--- src/zadania3/Zadania3.java
+++ src/zadania3/Zadania3.java
@@ -4,10 +4,34 @@ import java.util.Scanner;
 
 public class Zadania3 {
 
-    public void SprawdzeniePalindromu(String wyraz, String wyraz2) {
-
+    public static void sprawdzeniePalindromu(String wyraz) {
+        String odwracanie = new StringBuilder(wyraz).reverse().toString();
+        String wyraz2 =  wyraz.replace(" ","");
+        String odwrocony2 = odwracanie.replace(" ", "");
+        System.out.println(wyraz2);
+        System.out.println(odwrocony2);
+        if (wyraz2.equalsIgnoreCase(odwrocony2)) {
+            System.out.println("Wyrażenie jest palindromem");
+        }
+        else System.out.println("Wyrażenie nie jest palindromem");
     }
 
+    public static void utworzenieLiczb () {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        for (a = 1; a <= 4; a++) {
+            for (b = 1; b <= 4; b++) {
+                for (c = 1; c <= 4; c++) {
+                    if (a != b && b != c && c != a) {
+
+                        System.out.println(a+""+b+""+c);
+                    }
+                }
+            }
+        }
+    }
 
 
 
@@ -15,8 +39,6 @@ public class Zadania3 {
         Scanner wejscie = new Scanner(System.in);
         String wyraz = wejscie.nextLine();
         System.out.println(wyraz);
-        String odwracanie = new StringBuilder(wyraz).reverse().toString();
-        System.out.println(odwracanie);
         char [] w =  wyraz.toCharArray();
         int counter_upper = 0;
         int counter_lower = 0;
@@ -36,18 +58,11 @@ public class Zadania3 {
         System.out.println("Spacje = " + counter_space);
         System.out.println("Znak specjalny = " + special);
 
-        String wyraz2 =  wyraz.replace(" ","");
-        String odwrocony2 = odwracanie.replace(" ", "");
-        System.out.println(wyraz2);
-        System.out.println(odwrocony2);
-        boolean test = false;
-
-        if (wyraz2.equalsIgnoreCase(odwrocony2)) {
-            System.out.println("Wyrażenie jest palindromem");
-        }
-        else System.out.println("Wyrażenie nie jest palindromem");
 
 
+
+        sprawdzeniePalindromu(wyraz);
+        utworzenieLiczb();
     }
 
 
